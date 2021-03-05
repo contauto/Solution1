4f6ec643006bb0f660d9a669c442e9c51a0ef8b5
contauto <76818792+contauto@users.noreply.github.com>
2021-03-05 22:24:08 +0300

    Add project files.

---
+++ Business/Concrete/BookManager.cs
@@ -0,0 +1,57 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DataAccess.Concrete.EntityFramework;
+using System.Linq;
+
+namespace Business.Concrete
+{
+    public class BookManager : IBookService
+    {
+        private IBookDal _bookdal;
+
+        public BookManager(IBookDal bookDal)
+        {
+            _bookdal = bookDal;
+        }
+
+
+        public void Add(Book book)
+        {
+            if (_bookdal.Get(b => b.Name == book.Name&&b.AuthorId==book.AuthorId) == null)
+            {
+                _bookdal.Add(book);
+            }
+            else { throw new Exception("Bu kitap adı zaten mevcut"); }
+            
+        }
+
+        public void Delete(Book book)
+        {
+            _bookdal.Delete(book);
+        }
+
+        public List<Book> GetAll()
+        {
+            return _bookdal.GetList().ToList();
+        }
+
+        public List<Book> GetByAuthorId(int id)
+        {
+            return _bookdal.GetList(b => b.AuthorId == id).ToList();
+        }
+
+        public Book GetById(int id)
+        {
+            return _bookdal.Get(b => b.Id == id);
+        }
+
+        public void Update(Book book)
+        {
+            _bookdal.Update(book);
+        }
+    }
+}
